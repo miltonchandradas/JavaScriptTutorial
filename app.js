cb22e5d514f972fd14bf7c895310c08044ad5f04
Milton Chandradas <milton.chandradas@live.com>
2021-12-19 00:51:38 -0500

    primitive data types...

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("JavaScript Tutorial...")
+let myNumber = 3;
+let myString = `Milton`;
+let myBoolean = true;
+let myNull = null;
+let myUndefined;
+
+
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+console.log(myNull);
+console.log(myUndefined);
